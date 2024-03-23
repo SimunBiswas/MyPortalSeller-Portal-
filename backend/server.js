c1c46506aa332c421ebdab86da5a911f7c7264a0
ramprasanth0 <88130479+ramprasanth0@users.noreply.github.com>
2024-03-23 23:29:57 +0530

    updated root folder

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const dotenc = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//connect to DB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("DB connected")
+        app.listen(PORT, () => {
+            console.log(`server running on port${PORT}`)
+        });
+    })
+    .catch((err) => { console.log(err) })
