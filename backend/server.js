94711731640e8bd394d288572b5b3ecdb6bbfece
Ram Prasanth <essrramprasanth@gmail.com>
2024-03-23 23:49:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const dotenc = require("dotenv").config();
+const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
