83d10236f27f0a62fad0cd6524ee1dca102e5599
Saurabh Jaiswal <132423663+Saur7379@users.noreply.github.com>
2024-04-09 16:47:30 +0530

    Add files via upload

---
+++ ImagesDemo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import revogue from './images/revogue.jpg'
+
+function ImagesDemo() {
+  return (
+    <>
+    <div>
+     
+     <img src={revogue} class="center" height='100' width='150' />
+     </div> 
+    </>
+  )
+}
+
+export default ImagesDemo
