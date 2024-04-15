0f05a953a7e1b722d8b670b41e801e22238c849b
Atharv Dalal <sangamdalal24@gmail.com>
2024-04-15 10:54:20 +0530

    Separated Database Connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const dotenv = require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { dbConnection } from "./DB/dbConnection.js";
 
 const app = express();
 
@@ -18,13 +18,11 @@ app.get('/', (req, res) => {
     res.send("Hello world");
 });
 
-//Connect to DB
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("DB connected")
-        app.listen(PORT, () => {
-            console.log(`server running on port${PORT}`)
-        });
-    })
-    .catch((err) => { console.log(err) })
+
+
+app.listen((PORT, ()=> console.log(`Server is Running On ${PORT}`)))
+
+
+//Database Connection
+
+dbConnection()
