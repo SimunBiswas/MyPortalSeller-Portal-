790dcfeefd3c22a086f647ac4503822698bb3b48
Kuldeepthedev <124391385+Kuldeepthedev@users.noreply.github.com>
2024-04-16 13:37:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors())
 
 //Routes
 app.use('/api/v6', orders);
