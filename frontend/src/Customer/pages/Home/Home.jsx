64e0721740896841032d9cb46f82f932aa8a48c8
Faizan <faizan140798@gmail.com>
2024-04-17 13:32:51 +0530

    home!

--- frontend/src/Customer/pages/Home/Home.jsx
+++ frontend/src/Customer/pages/Home/Home.jsx
@@ -1,25 +1,24 @@
-// have to import diffrent components from component and page have to 
-// responsiable 
+// have to import diffrent components from component and page have to
+// responsiable
 
-import React from 'react'
-import Banner1 from './Banner1'
-import Banner2 from './Banner2'
-import Banner3 from './Banner3'
-import CategoryBanner from './CategoryBanner'
+import React from "react";
+import Banner1 from "./Banner1";
+import Banner2 from "./Banner2";
+import Banner3 from "./Banner3";
+import CategoryBanner from "./CategoryBanner";
 
 function Home() {
   return (
     <>
-    <div>
-      Home
-      <CategoryBanner/>
-      <Banner1/>
-      <Banner2/>
-      <Banner3/>
-    </div>
-    
+      <div>
+        Home !
+        <CategoryBanner />
+        <Banner1 />
+        <Banner2 />
+        <Banner3 />
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
