fb0ee5ac27447af951ebb5b6221eb577347b22c8
Parna Jain <110488462+parna25@users.noreply.github.com>
2024-04-17 21:44:34 +0530

    Update cartSchema.js

--- backend/Database/Models/UserDatabaseModel/cartSchema.js
+++ backend/Database/Models/UserDatabaseModel/cartSchema.js
@@ -1,30 +1,39 @@
 // have to create cart model like product deatil,userid, etc
 import mongoose from 'mongoose';
 
-
 const cartSchema = new mongoose.Schema({
-    userId: {
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [{
+    productId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: 'Product',
       required: true
     },
-    items: [{
-      productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: true
-      },
-      productName: {
-        type: String,
-        required: true
-      },
-      price: {
-        type: Number,
-        required: true
-      },
-      quantity: {
-        type: Number,
-        default: 1
-      }
-    }]
-  }, { timestamps: true });
+    productName: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    quantity: {
+      type: Number,
+      default: 1
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      required: true
+    }
+  }]
+}, { timestamps: true });
+
+export default mongoose.model('Cart', cartSchema);
