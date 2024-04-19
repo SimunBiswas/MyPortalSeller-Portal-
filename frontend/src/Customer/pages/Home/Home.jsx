8cddd3ca5b9f2dbc99a06d89cdb579048328b816
Faizan <faizan140798@gmail.com>
2024-04-19 22:44:07 +0530

    Home !

--- frontend/src/Customer/pages/Home/Home.jsx
+++ frontend/src/Customer/pages/Home/Home.jsx
@@ -6,14 +6,13 @@ import Banner1 from "./Banner1";
 import Banner2 from "./Banner2";
 import Banner3 from "./Banner3";
 import BottomCategoryBanner from "./BottomCategoryBanner";
-import CategoryBanner from './CategoryBanner'
-
+import CategoryBanner from "./CategoryBanner";
 
 function Home() {
   return (
     <>
       <div>
-        <h3>Home </h3>
+        <h3>Home!</h3>
         <CategoryBanner />
         <Banner1 />
         <Banner2 />
