0187a20309884640f318a22c98d4f2bafa7fde5c
Ajay Borde <97509897+AjayBorde91@users.noreply.github.com>
2024-04-22 00:18:04 +0530

    Banner2 : banner 2 updated .

--- frontend/src/Customer/pages/Home/Banner2.jsx
+++ frontend/src/Customer/pages/Home/Banner2.jsx
@@ -7,11 +7,11 @@ const categories = [
   { id: 2, name: 'INDIE', href: "https://www.ajio.com/shop/indie" },
   { id: 3, name: 'HOME', href: "https://www.ajio.com/shop/indie" },
   { id: 4, name: 'AJIO SNIKER HOOD', href: "https://www.ajio.com/shop/indie" },
-  { id: 5, name: 'WEDDING STORE', href: "https://www.ajio.com/shop/indie" },
+  { id: 5, name: ' Wedding Store', href: "https://www.ajio.com/shop/indie" },
   { id: 6, name: 'SAREE STORE', href: "https://www.ajio.com/shop/indie" },
-  { id: 7, name: 'WATCHES', href: "https://www.ajio.com/shop/indie" },
-  { id: 8, name: 'WESTERNWEAR', href: "https://www.ajio.com/shop/indie" },
-  { id: 9, name: 'ACCESSORIES', href: "https://www.ajio.com/shop/indie" },
+  { id: 7, name: 'watches', href: "https://www.ajio.com/shop/indie" },
+  { id: 8, name: 'western', href: "https://www.ajio.com/shop/indie" },
+  { id: 9, name: 'accessary', href: "https://www.ajio.com/shop/indie" },
   { id: 10, name: 'BEAUTY', href: "https://www.ajio.com/shop/indie" },
   { id: 11, name: 'INTERNATIONAL BRANDS', href: "https://www.ajio.com/shop/indie" },
   { id: 12, name: 'HOMEGROOUN BRANDS', href: "https://www.ajio.com/shop/indie" },
@@ -22,8 +22,8 @@ const Banner2 = () => {
     const category = categories.find(cat => cat.id === categoryId);
     if (category) {
       return (
-        <div key={category.id}>
-          <Link className='text-dark' to={category.href}>{category.name}</Link>
+        <div key={category.id} style={{textAlign:'center'}}>
+          <Link className='text-light ' to={category.href}>{category.name}</Link>
         </div>
       );
     }
