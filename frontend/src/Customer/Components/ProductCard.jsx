c52ba7d0dd6c06dd580a1d403edda8d8193fd035
Simun Biswas <simunbiswas14395@gmail.com>
2024-04-22 12:25:52 +0530

    images deleted

--- frontend/src/Customer/Components/ProductCard.jsx
+++ frontend/src/Customer/Components/ProductCard.jsx
@@ -2,47 +2,50 @@
 // have to function to when click card  redirect to 'product/:id'
 
 
-import React from 'react'
-import './styles/ProductCard.css';
+// import React from 'react'
+// import './styles/ProductCard.css';
 
-const products = [
-  { id: 1,imageUrl:'./Images/img1.jpg', title: 'Product 1', price: 10 },
-  { id: 2,imageUrl:'https://via.placeholder.com/250x250', title: 'Product 2', price: 20 },
-  { id: 3,imageUrl:'https://via.placeholder.com/250x250', title: 'Product 3', price: 30 },
-  {id:4,imageUrl:'https://via.placeholder.com/250x250', title: 'product 4' , price:40},
-];
+// const products = [
+//   { id: 1,imageUrl:'./Images/img1.jpg', title: 'Product 1', price: 10 },
+//   { id: 2,imageUrl:'https://via.placeholder.com/250x250', title: 'Product 2', price: 20 },
+//   { id: 3,imageUrl:'https://via.placeholder.com/250x250', title: 'Product 3', price: 30 },
+//   {id:4,imageUrl:'https://via.placeholder.com/250x250', title: 'product 4' , price:40},
+// ];
 
-const ProductCard = ({product}) => {
-  return (
+// const ProductCard = ({product}) => {
+//   return (
    
- <div className='container'>
-    <div className="product-card">
-      <img src={product.imageUrl} alt={product.title} className="product-image" />
-      <div className="product-details">
-        <h2 className="product-title">{product.title}</h2>
-        <p className="product-price">$Price: {product.price}</p>
-        <button className="add-to-cart-button">Add to Cart</button>
-      </div>
+//  <div className='container'>
+//     <div className="product-card">
+//       <img src={product.imageUrl} alt={product.title} className="product-image" />
+//       <div className="product-details">
+//         <h2 className="product-title">{product.title}</h2>
+//         <p className="product-price">$Price: {product.price}</p>
+//         <button className="add-to-cart-button">Add to Cart</button>
+//       </div>
+//     </div>
+//     </div>
+// );
+// };
+// const ProductList = () => (
+//   <div className="product-list">
+//     {products.map(product => (
+//       <ProductCard key={product.id} product={product} />
+//     ))}
+//   </div>
+// );
+
+
+// export default ProductList;
+
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      ProductCard
     </div>
-    </div>
-);
-};
-const ProductList = () => (
-  <div className="product-list">
-    {products.map(product => (
-      <ProductCard key={product.id} product={product} />
-    ))}
-  </div>
-);
+  )
+}
 
-
-    
-
-
-
-
-
- 
-
-
-export default ProductList;
+export default ProductCard
