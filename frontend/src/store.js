dc5e3a1ca0bee0d35e84ab4b9f2fcd21a6c7b673
Simun Biswas <simunbiswas14395@gmail.com>
2024-04-23 20:12:42 +0530

    3 units of reducers added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk'; 
+import thunk from 'redux-thunk';
+import productReducer from './productReducer'; // Import your product reducer
+import filterReducer from './filterReducer'; // Import your filter reducer
+import cartReducer from './cartReducer'; // Import your cart reducer
 
-
-
-const reducer = combineReducers({
-  
+// Combine your reducers
+const rootReducer = combineReducers({
+  product: productReducer,
+  filter: filterReducer,
+  cart: cartReducer, // Add cart reducer to rootReducer
 });
 
-const store = createStore(reducer, applyMiddleware(thunk));
+// Create the store with combined reducers and applyMiddleware for thunk
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 export default store;
